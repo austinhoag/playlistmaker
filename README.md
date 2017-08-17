2fce39da16c817d68306bbdd855d1675c507db3d
athoag <athoag@ucdavis.edu>
2017-08-16 23:04:54 -0700

    clean up

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 Makes a recommended iTunes playlist from an existing iTunes library 
 
 ## REQUIREMENTS
-sqlalchemy - python package to create and interact with an SQLite database, can be installed with pip
+sqlalchemy - python package which is used to create and interact with an SQLite database
+can be installed with pip, e.g. pip install sqlalchemy
 
 An iTunes Library xml file - export this from iTunes with File->Library->Export Library
 
--- setup.py
+++ setup.py
@@ -5,24 +5,6 @@ from imp import find_module
 try: find_module('sqlalchemy')
 except: sys.exit('### Error: python module sqlalchemy not found')
 
-# setup(name='iTunes_custom_playlist',
-#       version='1.0',
-#       author='Austin Hoag',
-#       author_email='austinthomashoag@gmail.com',
-#       url='https://github.com/athoag/playlistmaker',
-#       packages=['playlistmaker'],
-#       package_dir = {'':'src'}
-#       )
-
-# setup(name='Distutils',
-#       version='1.0',
-#       description='Python Distribution Utilities',
-#       author='Greg Ward',
-#       author_email='gward@python.net',
-#       url='https://www.python.org/sigs/distutils-sig/',
-#       packages=['distutils', 'distutils.command'],
-#      )
-
 setup(name='iTunes_custom_playlist',
       version='1.0',
       author='Austin Hoag',
